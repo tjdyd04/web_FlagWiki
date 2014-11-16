232d37195be9801af50e94a24e5952629706101c
jyk619 <realdragon619@gamil.com>
2014-11-17 01:56:49 +0900

    add Button

--- CheckTree/MainTree.js
+++ CheckTree/MainTree.js
@@ -1,18 +1,16 @@
-   $(document).ready(function() {
+    $(document).ready(function() {
 		$.getJSON('jsontest.jsp',function(data){
 		var html = '';
 		var leaf_num;
         $.each(data, function(entryIndex, entry) {
 			if(entry.branch==0){
 				html += '<ul>';
-				html += '<li class="collapsed"><input type="checkbox"><span idx=' + entry.idx+ '>' + entry.title + '</span><input type="hidden" id="idx' + entry.idx + '"' + ' value=' + entry.idx +'>';
-				html += '<button type="button"  class="btn btn-default btn-xs" num="' + entry.branch_num + '" Btype="tree" Btitle="' + entry.title + '"><span class="glyphicon glyphicon-chevron-down"></span></button>';
+				html += '<li class="collapsed"><input type="checkbox"><span idx=' + entry.idx+ '>' + entry.title + '</span><input type="hidden" id="idx' + entry.idx + '"' + ' value=' + entry.branch +'>';
 			}
 			if(entry.branch!=0 && entry.leaf==0){
 				leaf_num = entry.leaf_num;
 				html += '<ul>';
 				html += '<li class="collapsed"><input type="checkbox"><span idx=' + entry.idx+ '>' + entry.title + '</span><input type="hidden" id="idx' + entry.idx + '"' + ' value=' + entry.idx +'>';
-				html += '<button type="button"  class="btn btn-default btn-xs" num="' + entry.leaf_num + '" Btype="branch" Btitle="' + entry.title + '"><span class="glyphicon glyphicon-chevron-down"></span></button>';
 				if(leaf_num !=0){
 					html += '<ul>';
 				}else{
@@ -26,6 +24,8 @@
 				}
 			}
 		});	
+
+
 		html += '</ul>';
 		$('#tree').html(html);
        	$('#tree').tree({
@@ -43,23 +43,14 @@
             },
             select: function(event, element) {
                jidx = $(element).find('span.daredevel-tree-label:first').attr('idx');
-               $.post('jsBoard.jsp',
+               $.get('Testpage.jsp',
                      {idx: $('#idx'+jidx).val()},
                      function(data){
                         $('#right').html(data);
           		     });
+                    
             	}
+            
         	});
-			
-		$('#tree button').click(function(){
-			var type = $(this).attr('Btype');
-			var num = $(this).attr('num');
-			var title =$(this).attr('Btitle');
-			html = '<span>선택항목 : ' + title + '</span>';
-			html += '<input type="text" class="form-control" placeholder="추가하기" id="AddTree">'; 
-			html += '<input type="button" class="btn default" id="AddButton" value="추가">';
-			$('#input').html(html).toggle();
-
-			});
   		});
-	});
+	});	
--- CheckTree/jsBoard.jsp
+++ CheckTree/jsBoard.jsp
@@ -9,8 +9,8 @@
 <title>게시판 등록</title>
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js"></script>
 <script type="text/javascript" src="/ckeditor_full/ckeditor.js"></script>
-<script type="text/javascript" src="Button.js"></script>
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+<script src="Button.js"></script>
 <script type="text/javascript">
 var idx="<%=idx%>";
 $(document).ready(function(){
@@ -28,9 +28,7 @@ $(document).ready(function(){
 </script>
 </head>
 <body>
-<form id="AjaxForm">
 <div id="board"></div>
 <input type="button" id="button" value="적용" class="btn btn-default">
-</form>
 </body>
 </html>
